850c67b409436e98059bc7883913ae71d993aab9
Scorpion27S <Koklyuhin-gleb@mail.ru>
2025-02-20 22:09:35 +0700

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    public String surname;
+    private int sizeOfEmpty = 10;
+
+    void method(int a, int b, int c, int d, int e, int f) {
+    }
+
+    void echo() {
+        String name;
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+        Broken() {
+        }
+    }
