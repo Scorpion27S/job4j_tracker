ca825b6546fceb893259f5ece329eef67155ab72
Scorpion27S <Koklyuhin-gleb@mail.ru>
2025-02-26 21:41:39 +0700

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 public class Item {
+
     private int id;
     private String name;
 
@@ -18,5 +19,21 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Пусто ");
+        System.out.println("Имя: " + name);
+        System.out.println("ID + Имя: " + id + " " + name);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item(258, " Имя ");
+        item.printInfo();
     }
 }
